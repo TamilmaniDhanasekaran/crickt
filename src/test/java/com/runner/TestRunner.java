aaa153450e8b35e4d9656d39f26ca3c0e52134ae
TamilmaniDhanasekaran <68770218+TamilmaniDhanasekaran@users.noreply.github.com>
2020-07-26 22:56:00 +0530

    Update TestRunner.java

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "/Users/dtm/Desktop/cricbuzzUI/src/test/java/com/features/cricbuzz.feature", 
 		glue = {"com/stepDefinitions" },
-     tags= {"@Australiaseries"})
+     tags= {"@Cricbuzzschedule"})
 
 
 //@Cricbuzzschedule
